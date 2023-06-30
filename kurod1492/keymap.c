d6763f3b98d753b0a397230d1573476511b76ae9
Akihiro Kurotani <kurod1492@gmail.com>
2023-06-30 16:28:58 +0900

    modify keymap

--- kurod1492/keymap.c
+++ kurod1492/keymap.c
@@ -72,10 +72,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
 
 [_QWERTY] = LAYOUT(
-      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-      KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                            KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                            KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
-      ADJUST,  KC_ESC,  KC_SUU,  KC_LALT, KC_LGUI, KC_LSFT, LT(_LOWER, KC_SPC), LT(_RAISE, KC_ENT), KC_LBRC, KC_RBRC, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                 KC_Y,                 KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+      KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                 KC_H,                 KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                 KC_N,                 KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
+      ADJUST,  KC_LBRC, KC_RBRC, KC_LALT, KC_LGUI, LT(_LOWER, KC_LANG2), KC_SPC, KC_ENT, LT(_RAISE, KC_LANG1), KC_RGUI, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
       ),
   /* Colemak
    * ,-----------------------------------------.             ,-----------------------------------------.
@@ -125,10 +125,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-------------------------------------------------------------------------------------------------'
    */
   [_LOWER] = LAYOUT(
-      KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
-      KC_LCTL, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, KC_GRV,
-      KC_LSFT, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,                    KC_B,    _______, KC_PSCR, KC_HOME, KC_END,  KC_PIPE,
-      _______, KC_ESC,  KC_SUU,  KC_LALT, KC_LGUI, KC_LSFT, LOWER, RAISE,     KC_LPRN, KC_RPRN, _______, KC_VOLD, KC_VOLU, _______
+      KC_ESC,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD,
+      KC_LCTL, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_UNDS, KC_MINS, KC_EQL,  KC_PLUS, _______, KC_GRV,
+      KC_LSFT, _______, _______, _______, _______, _______,                   KC_B,    _______, KC_PSCR, KC_HOME, KC_END,  KC_PIPE,
+      _______, KC_LBRC, KC_RBRC, KC_LALT, KC_LGUI, LOWER,   KC_LBRC, KC_RBRC, RAISE,   KC_RGUI, _______, KC_VOLD, KC_VOLU, _______
       ),
 
   /* Raise
@@ -143,10 +143,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-------------------------------------------------------------------------------------------------'
    */
   [_RAISE] = LAYOUT(
-      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-      KC_LCTL, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_UNDS, KC_EQL,  KC_LBRC, KC_RBRC, KC_TILD,
-      KC_LSFT, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,                    KC_F12,  KC_MINS, KC_PLUS, KC_PGDN, KC_PGUP, KC_PIPE,
-      _______, KC_ESC,  KC_SUU,  KC_LALT, KC_LGUI, KC_LSFT, LOWER, RAISE,     KC_LPRN, KC_RPRN, KC_HOME, KC_PGDN, KC_PGUP, KC_END
+      KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+      KC_LCTL, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_F12,
+      KC_LSFT, _______, _______, _______, _______, _______,                   _______, _______, _______, KC_DOT,  KC_SLSH, _______,
+      _______, KC_LBRC, KC_RBRC, KC_LALT, KC_LGUI, LOWER,   KC_LBRC, KC_RBRC, RAISE,   KC_RGUI, _______, _______, _______, _______
       ),
 
   /* Adjust (Lower + Raise)
